89d5646757a31e94d801629a105265fb2ed4b487
Brian <18603393+brian6932@users.noreply.github.com>
2024-08-08 13:04:41 -0400

    feat: Inline param stripping

--- linkfix.js
+++ linkfix.js
@@ -4,17 +4,44 @@
 
 let i = -1
 const
+	strippableParams = [
+		"ei",
+		"fbs",
+		"sa",
+		"sca_esv",
+		"sca_upv",
+		"source",
+		"sxsrf",
+		"ved",
+	],
 	links = document.links,
 	fixLinks = () => {
 		while (++i < links.length) {
-			if (links[i].pathname !== "/url" || links[i].search === "")
+			if (links[i].search === "" && links[i].host !== "www.google.com")
 				continue
-			// Extract the search without the leading '?'.
-			const q = new URLSearchParams(links[i].search.slice(1)).get("q")
-			// note to self: must change href attribute, not the entire
-			// thing. Doh. Read the docs, sort of.
-			if (q !== null)
-				links[i].href = decodeURIComponent(q)
+
+			switch (links[i].pathname) {
+				case "/url":
+					// Extract the search without the leading '?'.
+					const q = new URLSearchParams(links[i].search.slice(1)).get("q")
+					// note to self: must change href attribute, not the entire
+					// thing. Doh. Read the docs, sort of.
+					if (q !== null)
+						links[i].href = decodeURIComponent(q)
+				case "/":
+				case "/maps":
+				case "/preferences":
+				case "/search":
+				case "/setprefs":
+				case "/travel":
+				case "/webhp":
+					const link = new URL(links[i].href)
+
+					for (const param of strippableParams)
+						link.searchParams.delete(param)
+
+					links[i].href = link.toString()
+			}
 		}
 	}
 
